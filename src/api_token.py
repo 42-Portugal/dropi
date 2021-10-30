8178de61afcdde8a21ed5e3b3cba141d272e184f
Pedro Mora - pemora/drop <drop@42lisboa.com>
2021-10-30 22:20:30 +0100

    adds params to ApiToken.__init__()
    ApiToken now can be initialized with a uid and secret defined by params
    instead of env. Env can still be used if no params are supplied

--- src/api_token.py
+++ src/api_token.py
@@ -4,11 +4,15 @@ from . import config
 
 
 class ApiToken:
-    def __init__(self):
+    def __init__(self, uid: str="", secret: str=""):
+        self.__params = config.params.copy()
+        if uid and secret:
+            self.__params['client_id'] = uid
+            self.__params['client_secret'] = secret
         self.json = self.get()
 
     def get(self):
-        resp = requests.post(config.token_url, config.params)
+        resp = requests.post(config.token_url, self.__params)
         status = resp.status_code
         resp.raise_for_status()
         return resp.json()
