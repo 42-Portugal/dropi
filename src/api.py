b0075c2a1bd70545555cecc8622f60672352c60a
Pedro Mora - pemora/drop <drop@42lisboa.com>
2021-10-30 04:46:27 +0100

    fixes typo

--- src/api.py
+++ src/api.py
@@ -29,7 +29,7 @@ class LogLvl(IntEnum):
 
 
 class Api42:
-    def __init__(self, token: config.ApiToken,
+    def __init__(self, token: api_token.ApiToken,
             log_lvl: LogLvl = LogLvl.Debug, raises: bool = True):
         self.token = token
         self.log_lvl = log_lvl
