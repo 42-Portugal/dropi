54693c6ff8162c1e7700db5968c4b207e17bc06e
Drop - Pedro Mora <drop@42lisboa.com>
2022-01-18 14:20:55 +0000

    add instal requires to setup.py, fix py version

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 import setuptools
+import pathlib
+import pkg_resources
+
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -20,5 +29,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     packages=['dropi'],
-    python_requires=">=3.9",
+    python_requires=">=3.10",
+    install_requires=install_requires
 )
