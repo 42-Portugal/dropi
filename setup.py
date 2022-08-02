1c0c7e1494a83385f657500275c3ae217a8df93a
drop <drop@42lisboa.com>
2022-08-02 19:55:45 +0100

    fixed requirements, renamed dropi/ to src/

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     version="0.42",
     author="Pedro Mora - \"drop\"",
     author_email="drop@42lisboa.com",
-    description="A (small) 42 api connector library",
+    description="A (small) api connector library for 42's intranet",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/42-Lisboa/dropi",
@@ -29,6 +29,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     packages=['dropi'],
+    package_dir={'dropi': 'src'},
     python_requires=">=3.10",
     install_requires=install_requires
 )
